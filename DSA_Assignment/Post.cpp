9ba256e3fb1dee6c284b09f68bb0ea79fb399bc2
koayyiting <s10221765@connect.np.edu.sg>
2023-02-05 18:30:07 +0800

    Post class

--- DSA_Assignment/Post.cpp
+++ DSA_Assignment/Post.cpp
@@ -3,10 +3,12 @@
 #include "Post.h"
 
 Post::Post() { }
-Post::Post(string t, string c, time_t pt) : title(t), content(c), postTime(pt) { }
+Post::Post(string tp, string c, string pt,string un) : title(tp), content(c), postTime(pt),username(un) { }
 void Post::setPostTitle(string t) { title = t; }
 string Post::getPostTitle() { return title; }
 void Post::setPostContent(string c) { content = c; }
 string Post::getPostContent() { return content; }
-void Post::setPostTime(time_t pt) { postTime = pt; }
-time_t Post::getPostTime() { return postTime; }
+void Post::setPostTime(string pt) { postTime = pt; }
+string Post::getPostTime() { return postTime; }
+void Post::setUsername(string un) { username = un; }
+string Post::getUsername() { return username; }
